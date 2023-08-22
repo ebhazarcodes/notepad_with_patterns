c1ed233c38947716530a39d6d0df6ae0993151fa
ebhazarcodes <erdoganbaranhazar@gmail.com>
2023-08-22 16:48:05 +0300

    add new button properties

--- src/notepadProject/notepd.java
+++ src/notepadProject/notepd.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionEvent;
 import java.awt.GridLayout;
 import javax.swing.BoxLayout;
 import javax.swing.JTextField;
+import java.awt.Button;
 
 public class notepd extends JFrame {
 
@@ -44,6 +45,59 @@ public class notepd extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.PAGE_AXIS));
+		
+		JButton btnNewButton = new JButton("Ac");
+		
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JPanel buttonPanel = new JPanel( new GridLayout(0, 3, 6, 5) );
+	    buttonPanel.setBorder( new EmptyBorder(338, 233, 0, 0) );
+
+	    buttonPanel.setOpaque( false );
+
+
+	    //for (int i = 0; i < 7; i++)
+	    {
+	        JButton button = new JButton("Button");
+	        JButton button1 = new JButton("Button");
+	        JButton button2 = new JButton("Button");
+	        JButton button3 = new JButton("Button");
+	        JButton button4 = new JButton("Button");
+	        JButton button5 = new JButton("Button");
+
+	        button.setPreferredSize( new Dimension(160, 45) );
+	        buttonPanel.add(button);
+	        buttonPanel.add(button1);
+	        buttonPanel.add(button2);
+	        buttonPanel.add(button3);
+	        buttonPanel.add(button4);
+	        buttonPanel.add(button5);
+
+	         button.addActionListener(new Action());
+	    }
+		
+		JButton btnNewButton_1 = new JButton("Kapat");
+		
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Kaydet");
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Farkl\u0131 Kaydet");
+		contentPane.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("Bul");
+		contentPane.add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("Geri Al");
+		contentPane.add(btnNewButton_5);
 		
 		textField = new JTextField();
 		contentPane.add(textField);
